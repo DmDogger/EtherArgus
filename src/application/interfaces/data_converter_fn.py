49e25b539cad1bce652b60500fedfed05a5e0cc3
Dmitrii Kotik <dmdoggx@gmail.com>
2026-05-01 18:00:29 +0700

    refactor: small refactor

--- src/application/interfaces/data_converter_fn.py
+++ src/application/interfaces/data_converter_fn.py
@@ -8,6 +8,4 @@ from application.interfaces.feature_extraction import BuiltFeatures
 
 
 class FromRawDictToDataFrame(Protocol):
-    """Вызываемый объект: словарь признаков (как у директора) → ``pandas.DataFrame``."""
-
     def __call__(self, data: BuiltFeatures) -> pd.DataFrame: ...
